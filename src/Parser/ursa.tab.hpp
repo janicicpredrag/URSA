9d59c165e9ab2ba53e905a5e8d9a24d29c03d0dc
Predrag Janicic <janicic@matf.bg.ac.rs>
2022-03-11 14:02:38 +0100

    small change in the parser

--- src/Parser/ursa.tab.hpp
+++ src/Parser/ursa.tab.hpp
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.5.1.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -29,6 +30,9 @@
 
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
+
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
 
 #ifndef YY_YY_URSA_TAB_HPP_INCLUDED
 # define YY_YY_URSA_TAB_HPP_INCLUDED
@@ -102,19 +106,18 @@ extern int yydebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
 union YYSTYPE
 {
-#line 22 "Parser/ursa.ypp" /* yacc.c:1909  */
+#line 22 "Parser/ursa.ypp"
 
     char* sValue;               /* integer value written as string */
     char  bValue;               /* boolean value */
     char* sName;                /* symbol table index */
     nodeType *nPtr;             /* node pointer */
 
-#line 116 "ursa.tab.hpp" /* yacc.c:1909  */
-};
+#line 119 "ursa.tab.hpp"
 
+};
 typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
